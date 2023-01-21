ae3e9d1a3543f81938d76c4b1168ac91b5243d48
Michael Migacev <micha.migacev@googlemail.com>
2023-01-21 14:58:08 +0100

    added STepperMotor GPIO for testing

--- app.py
+++ app.py
@@ -1,11 +1,40 @@
 # Installed
 import multiprocessing
 import sys, time
+import RPi.GPIO as GPIO
 
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from forms import AddItemForm, AddStepForm, EditItemForm
 from datetime import datetime, timedelta
+
+#raspberry GPIO
+
+GPIO.setmode(GPIO.BOARD)
+control_pins = [7,11,13,15]
+for pin in control_pins:
+  GPIO.setup(pin, GPIO.OUT)
+  GPIO.output(pin, 0)
+halfstep_seq = [
+  [1,0,0,0],
+  [1,1,0,0],
+  [0,1,0,0],
+  [0,1,1,0],
+  [0,0,1,0],
+  [0,0,1,1],
+  [0,0,0,1],
+  [1,0,0,1]
+]
+halfstep2_seq = [
+  [0,0,0,1],
+  [0,0,1,1],
+  [0,0,1,0],
+  [0,1,1,0],
+  [0,1,0,0],
+  [1,1,0,0],
+  [1,0,0,0],
+  [1,0,0,1]
+]
 
 
 app = Flask(__name__)
@@ -47,7 +76,12 @@ with app.app_context():
 
 def phys():
     while True:
-        print("hi")
+        for i in range(100):
+            for halfstep in range(8):
+                for pin in range(4):
+                    GPIO.output(control_pins[pin], halfstep2_seq[halfstep][pin])
+            time.sleep(0.001)
+        GPIO.cleanup()
         time.sleep(1)
 
 
