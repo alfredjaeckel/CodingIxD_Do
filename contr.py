ff849dea513dc60f55b3250969204ef04e304e5c
alfredjaeckel <alfred.thomas.jaeckel@gmail.com>
2023-01-21 15:34:12 +0100

    motor contoll fixes

--- contr.py
+++ contr.py
@@ -6,36 +6,37 @@ import time
 
 
 def move():
+    GPIO.setmode(GPIO.BOARD)
+    control_pins = [7, 11, 13, 15]
+    for pin in control_pins:
+        GPIO.setup(pin, GPIO.OUT)
+        GPIO.output(pin, 0)
+    halfstep_seq = [
+        [1, 0, 0, 0],
+        [1, 1, 0, 0],
+        [0, 1, 0, 0],
+        [0, 1, 1, 0],
+        [0, 0, 1, 0],
+        [0, 0, 1, 1],
+        [0, 0, 0, 1],
+        [1, 0, 0, 1]
+    ]
+    halfstep2_seq = [
+        [0, 0, 0, 1],
+        [0, 0, 1, 1],
+        [0, 0, 1, 0],
+        [0, 1, 1, 0],
+        [0, 1, 0, 0],
+        [1, 1, 0, 0],
+        [1, 0, 0, 0],
+        [1, 0, 0, 1]
+    ]
     while True:
-        GPIO.setmode(GPIO.BOARD)
-        control_pins = [7, 11, 13, 15]
-        for pin in control_pins:
-            GPIO.setup(pin, GPIO.OUT)
-            GPIO.output(pin, 0)
-        halfstep_seq = [
-            [1, 0, 0, 0],
-            [1, 1, 0, 0],
-            [0, 1, 0, 0],
-            [0, 1, 1, 0],
-            [0, 0, 1, 0],
-            [0, 0, 1, 1],
-            [0, 0, 0, 1],
-            [1, 0, 0, 1]
-        ]
-        halfstep2_seq = [
-            [0, 0, 0, 1],
-            [0, 0, 1, 1],
-            [0, 0, 1, 0],
-            [0, 1, 1, 0],
-            [0, 1, 0, 0],
-            [1, 1, 0, 0],
-            [1, 0, 0, 0],
-            [1, 0, 0, 1]
-        ]
+
         for i in range(100):
             for halfstep in range(8):
                 for pin in range(4):
                     GPIO.output(control_pins[pin], halfstep2_seq[halfstep][pin])
                 time.sleep(0.001)
-        GPIO.cleanup()
+    GPIO.cleanup()
 
