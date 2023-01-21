981fe64611d0d23ce179ada2fadc36153ee7019c
alfredjaeckel <alfred.thomas.jaeckel@gmail.com>
2023-01-21 15:19:25 +0100

    motor contoll fixes

--- contr.py
+++ contr.py
@@ -1,44 +1,50 @@
 import RPi.GPIO as GPIO
 import time
 
-#raspberry GPIO
 
+# raspberry GPIO
 
 
 def move():
-  GPIO.setmode(GPIO.BOARD)
-  control_pins = [7, 11, 13, 15]
-  for pin in control_pins:
-    GPIO.setup(pin, GPIO.OUT)
-    GPIO.output(pin, 0)
+    GPIO.setmode(GPIO.BOARD)
+    control_pins = [7, 11, 13, 15]
+    for pin in control_pins:
+        GPIO.setup(pin, GPIO.OUT)
+        GPIO.output(pin, 0)
 
-  halfstep_seq = [
-    [1, 0, 0, 0],
-    [1, 1, 0, 0],
-    [0, 1, 0, 0],
-    [0, 1, 1, 0],
-    [0, 0, 1, 0],
-    [0, 0, 1, 1],
-    [0, 0, 0, 1],
-    [1, 0, 0, 1]
-  ]
-  halfstep2_seq = [
-    [0, 0, 0, 1],
-    [0, 0, 1, 1],
-    [0, 0, 1, 0],
-    [0, 1, 1, 0],
-    [0, 1, 0, 0],
-    [1, 1, 0, 0],
-    [1, 0, 0, 0],
-    [1, 0, 0, 1]
-  ]
+    halfstep_seq = [
+        [1, 0, 0, 0],
+        [1, 1, 0, 0],
+        [0, 1, 0, 0],
+        [0, 1, 1, 0],
+        [0, 0, 1, 0],
+        [0, 0, 1, 1],
+        [0, 0, 0, 1],
+        [1, 0, 0, 1]
+    ]
+    halfstep2_seq = [
+        [0, 0, 0, 1],
+        [0, 0, 1, 1],
+        [0, 0, 1, 0],
+        [0, 1, 1, 0],
+        [0, 1, 0, 0],
+        [1, 1, 0, 0],
+        [1, 0, 0, 0],
+        [1, 0, 0, 1]
+    ]
 
 
-  while True:
-    for i in range(100):
-      for halfstep in range(8):
-        for pin in range(4):
-          GPIO.output(control_pins[pin], halfstep2_seq[halfstep][pin])
-      time.sleep(0.001)
-    GPIO.cleanup()
-    time.sleep(1)
+    while True:
+        for i in range(100):
+            for halfstep in range(8):
+                for pin in range(4):
+                    GPIO.output(control_pins[pin], halfstep2_seq[halfstep][pin])
+            time.sleep(0.001)
+        time.sleep(1)
+        for i in range(100):
+            for halfstep in range(8):
+                for pin in range(4):
+                    GPIO.output(control_pins[pin], halfstep_seq[halfstep][pin])
+            time.sleep(0.001)
+        time.sleep(1)
+
