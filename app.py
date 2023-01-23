1b6bebdf4725c007fe5b27f20240cb34bc5b2275
alfredjaeckel <alfred.thomas.jaeckel@gmail.com>
2023-01-23 13:37:15 +0100

    try button controll

--- app.py
+++ app.py
@@ -188,4 +188,4 @@ def achievement(ev=None):
 
 
 GPIO.setup(achieve_btn, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.add_event_detect(achieve_btn, GPIO.FALLING, callback=achievement(), bouncetime=20)
+GPIO.add_event_detect(achieve_btn, GPIO.FALLING, callback=achievement, bouncetime=20)
